6e62f225aa3f0bbc0f62e57e00e5dd3e926ced5e
Otto Andelin <oandelin@c3r5p5.hive.fi>
2023-08-14 14:56:56 +0300

    more readline experiments

--- playground/readline/prompt.c
+++ playground/readline/prompt.c
@@ -4,15 +4,22 @@
 #include "libft/include/libft.h"
 #include <stdlib.h>
 
-unsigned char *command_buf;
 
 int main()
 {
+
+	unsigned char *command_buf;
+
     while(1)
     {
-        command_buf = readline("test> ");
+        command_buf = readline("shell on earth> ");
         if (!ft_strncmp(command_buf, "exit", 4))
+		{
+			printf("Bye\n");
             break;
+		}
+		else 
+			printf("Command not found\n");
     }
     return 0;
 }

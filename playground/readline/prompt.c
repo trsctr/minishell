c46db88ecc0eadfa9a3dbac2a7e3d5055eb3dd65
trsctr <trisector@gmail.com>
2023-08-15 17:12:49 +0300

    testing readline history etc

--- playground/readline/prompt.c
+++ playground/readline/prompt.c
@@ -12,14 +12,24 @@ int main()
 
     while(1)
     {
-        command_buf = readline("shell on earth> ");
+        command_buf = readline("shell on earth : ");
         if (!ft_strncmp(command_buf, "exit", 4))
 		{
-			printf("Bye\n");
-            break;
+			ft_putendl_fd("exit", 2);
+            clear_history();
+            exit(0);
 		}
+        else if (!ft_strncmp(command_buf, "cd", 2) && command_buf[2] == ' ')
+        {
+            chdir(command_buf+3);
+        }
+        else if (!ft_strncmp(command_buf, "echo", 4) && (command_buf[4] == '\0' || command_buf[4] == ' '))
+        {
+            ft_printf("%s\n", command_buf+5);
+        }
 		else 
-			printf("Command not found\n");
+			ft_printf("%s: Command not found\n", command_buf);
+        add_history(command_buf);
     }
     return 0;
 }

12c836365b881f1b511120fadc50a4ad7408b81a
Otto Andelin <oandelin@c2r1p6.hive.fi>
2023-08-21 12:33:11 +0300

    added documentation of the prompt functions so far

--- src/prompt.c
+++ src/prompt.c
@@ -6,13 +6,21 @@
 /*   By: oandelin <oandelin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 15:24:25 by oandelin          #+#    #+#             */
-/*   Updated: 2023/08/19 14:36:37 by oandelin         ###   ########.fr       */
+/*   Updated: 2023/08/21 12:22:00 by oandelin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "prompt.h"
 
+/**
+ * @brief this function runs the command prompt. it receives user input from
+ * the function get_input which actually shows the command prompt, and will
+ * send it to lexer/parser. if we receive NULL from the get_input function,
+ * it means user has pressed CTRL+D and in that case we exit.
+ * 
+ * @param ms 
+ */
 void	prompt(t_ms *ms)
 {
 	char	*input;
@@ -31,6 +39,18 @@ void	prompt(t_ms *ms)
 			ft_printf("%s: %s\n", input, CMD_NOT_FOUND);
 	}
 }
+
+/**
+ * @brief displays the command prompt using readline to get the user input
+ * and listens to possible signals from the user, but doesn't display the signals
+ * on terminal.
+ * if user presses ctrl+d when the line is empty, readline receives EOF
+ * and in that case we return a NULL. if the command we receive is not empty
+ * we will save it to history with add_history function.
+ * in the end signal and terminal settings are reset and line is returned.
+ * 
+ * @return 
+ */
 
 char	*get_input(void)
 {

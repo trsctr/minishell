4eef172e47196cbb2333b44482b1e0675c8548fc
Otto Andelin <oandelin@c3r5p1.hive.fi>
2023-09-04 17:40:31 +0300

    added an printf-like function for printing error messages to stderr, and fixed a bug with handling ctrl+c

--- include/prompt.h
+++ include/prompt.h
@@ -6,16 +6,18 @@
 /*   By: oandelin <oandelin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 15:31:33 by oandelin          #+#    #+#             */
-/*   Updated: 2023/08/25 14:12:06 by oandelin         ###   ########.fr       */
+/*   Updated: 2023/09/04 17:15:00 by oandelin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PROMPT_H
 # define PROMPT_H
 # include <signal.h>
+# include <stdarg.h>
 # include <readline/history.h>
 # include <readline/readline.h>
 # include "builtins.h"
+
 
 void	prompt(t_data *data);
 char	*get_input(void);
@@ -24,5 +26,5 @@ void	handle_sig_int(int signal);
 void	toggle_echoctl(void);
 void	reset_signals(void);
 int		is_builtin(char *input);
-
+int		ft_printf_stderr(const char *format, ...);
 #endif
